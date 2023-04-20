f72b9227baf4d0e7b12e86726dea67688cee5f78
Ronnan Lima <94139723+ronnanlimao@users.noreply.github.com>
2023-04-19 21:19:32 -0300

    Add files via upload

---
+++ SQL challenge - sqlbolt-com.sql
@@ -0,0 +1,33 @@
+----------------------------------------------------- https://sqlbolt.com/lesson/select_queries_introduction -----------------------------------------------------
+-----------------------------------------------------> Exercício 1 — Tarefas --  SQL Lesson 1: SELECT queries 101 
+
+-- Encontre o title de cada filme
+select title from movies
+
+-- Encontre o director de cada filme
+select director from movies
+
+-- Encontre o titlee director de cada filme
+select title, director from movies
+
+-- Encontre o titlee year de cada filme
+select title, year from movies
+
+-- Encontre allas informações sobre cada filme
+select * from movies
+
+
+
+----------------------------------------------------- https://sqlbolt.com/lesson/select_queries_with_constraints -----------------------------------------------------
+-----------------------------------------------------> Exercício 2 — Tarefas --  SQL Lesson 2: Queries with constraints (Pt. 1) 
+
+-- Encontre o filme com uma linha id de 6
+
+
+-- Encontre os filmes lançados nos yeara nos de 2000 a 2010
+
+
+-- Encontre os filmes não lançados nos year anos entre 2000 e 2010
+
+
+-- Encontre os 5 primeiros filmes da Pixar e seus lançamentos year
